e4217180196d9deed60fc90aac7155aa11182f59
David O'Trakoun <me@davidosomething.com>
2015-11-16 23:16:47 -0500

    Initial commit with scaffolding

---
+++ gulpfile.js
@@ -0,0 +1,72 @@
+'use strict';
+
+var gulp        = require('gulp');
+var exec        = require('child_process').execSync;
+var sourcemaps  = require('gulp-sourcemaps');
+
+// =============================================================================
+// Require: CSS
+// =============================================================================
+var sass         = require('gulp-sass');
+var postcss      = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var mqpacker     = require('css-mqpacker');
+var cssnano      = require('cssnano');
+
+// =============================================================================
+// Require: Stream manip
+// =============================================================================
+var merge  = require('merge-stream');
+var concat = require('gulp-concat');
+
+// =============================================================================
+// Task: CSS
+// =============================================================================
+
+gulp.task('css', function () {
+
+  var globalSass = gulp
+    .src('./assets/scss/global.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss([
+      autoprefixer({
+        browsers: ['last 2 versions']
+      }),
+      cssnano
+    ]))
+    ;
+
+  var normalizeCss = gulp
+    .src('./assets/jspm/github/necolas/normalize.css@3.0.3/normalize.css')
+    ;
+
+  return merge(normalizeCss, globalSass)
+    .pipe(concat('global.css'))
+    .pipe(gulp.dest('./assets/css/'))
+    ;
+
+});
+
+// =============================================================================
+// Task: JS
+// =============================================================================
+gulp.task('js', function () {
+  exec('npm run js', function (err, stdout, stderr) {
+    if (err) {
+      throw err;
+    }
+    else {
+      console.log('Build complete!');
+    }
+  });
+});
+
+// =============================================================================
+// Task: Default
+// =============================================================================
+
+gulp.task('default', function () {
+
+  gulp.start('css');
+
+});
---
+++ index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>davidosomething.com</title>
+
+<!-- wp_print_stylesheets -->
+<link rel="stylesheet" href="assets/css/global.css">
+
+</head>
+<body>
+
+<header class="global__header">
+  <h1>davidosomething.com</h1>
+</header>
+
+<main>
+
+<article>
+
+  <header>
+    <h2>Article title goes here</h2>
+  </header>
+
+  <footer>
+    <time datetime="2015-05-16 19:00">2015-05-16</time>
+  </footer>
+
+</article>
+
+</main>
+
+<nav class="global__nav">
+</nav>
+
+<footer class="global__footer">
+</footer>
+
+
+<!-- wp_print_footer_scripts -->
+<script src="assets/jspm/system.js"></script>
+<script src="config.js"></script>
+<script>System.import('assets/js/dist/global.js');</script>
+</body>
+</html>
