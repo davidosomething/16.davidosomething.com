f01e4f0cddb1a0a6167fba88a86be7ef927d741a
David O'Trakoun <me@davidosomething.com>
2015-11-16 23:28:04 -0500

    some test content and article markup

--- index.php
+++ index.php
@@ -5,15 +5,10 @@
 <meta http-equiv="x-ua-compatible" content="ie=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>davidosomething.com</title>
-
-<!-- wp_print_scripts -->
 <script src="assets/js/dist/global.js" async></script>
-
-<!-- wp_print_stylesheets -->
 <link rel="stylesheet" href="assets/css/global.css">
-
 </head>
-<body>
+<body class="home">
 
 <header class="global__header">
   <h1>davidosomething.com</h1>
@@ -21,17 +16,68 @@
 
 <main>
 
-<article>
+  <article class="article--excerpt">
 
-  <header>
-    <h2>Article title goes here</h2>
-  </header>
+    <header class="article--excerpt__header">
+      <h2>Article title goes here</h2>
+    </header>
 
-  <footer>
-    <time datetime="2015-05-16 19:00">2015-05-16</time>
-  </footer>
+    <section classs="article--excerpt__body">
+      <p>Faucibus a maiores laborum cillum! Duis debitis vivamus aut proident
+      accusantium nemo, dolorem suscipit! Reiciendis? Commodi! Volutpat
+      possimus, tristique auctor tenetur venenatis ligula laoreet, tempora,
+      nobis massa! Totam vel dolorem minus phasellus perspiciatis! Minim aliqua
+      volutpat consequat possimus fugit accusantium, mauris repellat!
+      Accusantium quo parturient? Molestiae voluptatum ducimus, optio commodo
+      litora dis! Etiam tincidunt! In eiusmod! Litora parturient neque ullamco?
+      Inceptos proident, minima, magnis tortor qui congue donec aliquet wisi
+      rhoncus repellendus provident minima reprehenderit dicta, officia ab per
+      tortor.
+      </p>
 
-</article>
+      <p>Elit facilis aut augue potenti excepturi ex vehicula imperdiet harum
+      sunt expedita at voluptates massa, odit eget exercitationem adipisicing
+      mus laboris, eros aliquam sagittis! Reprehenderit? Ducimus excepteur
+      aperiam, dis occaecati? Sociosqu metus dis, metus! Cursus excepteur,
+      omnis risus. Mollis fermentum, fermentum labore ullam leo voluptas
+      eleifend doloribus molestias accumsan diamlorem aenean, euismod etiam
+      mauris, ducimus ipsa in inventore, ultricies nihil deserunt minus fugiat!
+      Quae quisque, nemo harum ridiculus sapien in ut reprehenderit, eaque
+      tempus at aenean nisi, ea, nibh perferendis.
+      </p>
+    </section>
+
+    <footer class="article--footer">
+      <time datetime="2015-05-16 19:00">2015-05-16</time>
+    </footer>
+
+  </article>
+
+
+  <article class="article--excerpt">
+
+    <header class="article--excerpt__header">
+      <h2>Article title goes here</h2>
+    </header>
+
+    <section classs="article--excerpt__body">
+      <p>Ipsam recusandae adipisicing, nostrud temporibus metus nemo. Wisi,
+      ante sapiente laudantium, tempor! Deleniti nam elementum? Distinctio
+      litora! Sed sodales adipiscing excepturi aute blandit laborum? Maiores id
+      incidunt, praesentium dictum sequi. Quod quaerat laboris eleifend
+      voluptate nullam mus repellat, lacus tempora! Animi malesuada,
+      consequatur accumsan odit rerum ex irure. Risus, rerum purus. Nihil!
+      Nulla fringilla hymenaeos anim assumenda id aspernatur cursus? Voluptates
+      quis ab nascetur, interdum quo, blanditiis optio quisquam totam, ducimus
+      habitasse, nostrud quas, minus adipisicing minima donec. Adipisicing quo.
+      </p>
+    </section>
+
+    <footer class="article--footer">
+      <time datetime="2015-05-16 19:00">2015-05-16</time>
+    </footer>
+
+  </article>
 
 </main>
 
