edc4790f66ad41c4a3de4b76ce7a66a540eaf8d6
David O'Trakoun <me@davidosomething.com>
2015-12-02 02:07:42 -0500

    flexbox structure

--- index.php
+++ index.php
@@ -86,12 +86,156 @@
 
       </article>
 
+      <article class="article article--excerpt">
+
+        <header class="article__header">
+          <h2>Article title goes here</h2>
+
+          <section class="article__meta">
+            <time datetime="2015-05-16 19:00">November 12, 2015</time>
+            <ul class="article__tags">
+              <li><span class="article__tag">tag 1</span></li>
+              <li><span class="article__tag">another</span></li>
+              <li><span class="article__tag">a longer tag</span></li>
+              <li><span class="article__tag">www</span></li>
+              <li><span class="article__tag">javascript</span></li>
+            </ul>
+          </section>
+        </header>
+
+        <section class="article__body">
+          <p>Faucibus a maiores laborum cillum! Duis debitis vivamus aut proident
+          accusantium nemo, dolorem suscipit! Reiciendis? Commodi! Volutpat
+          possimus, tristique auctor tenetur venenatis ligula laoreet, tempora,
+          nobis massa! Totam vel dolorem minus phasellus perspiciatis! Minim aliqua
+          volutpat consequat possimus fugit accusantium, mauris repellat!
+          Accusantium quo parturient? Molestiae voluptatum ducimus, optio commodo
+          litora dis! Etiam tincidunt! In eiusmod! Litora parturient neque ullamco?
+          Inceptos proident, minima, magnis tortor qui congue donec aliquet wisi
+          rhoncus repellendus provident minima reprehenderit dicta, officia ab per
+          tortor.
+          </p>
+
+          <p>Elit facilis aut augue potenti excepturi ex vehicula imperdiet harum
+          sunt expedita at voluptates massa, odit eget exercitationem adipisicing
+          mus laboris, eros aliquam sagittis! Reprehenderit? Ducimus excepteur
+          aperiam, dis occaecati? Sociosqu metus dis, metus! Cursus excepteur,
+          omnis risus. Mollis fermentum, fermentum labore ullam leo voluptas
+          eleifend doloribus molestias accumsan diamlorem aenean, euismod etiam
+          mauris, ducimus ipsa in inventore, ultricies nihil deserunt minus fugiat!
+          Quae quisque, nemo harum ridiculus sapien in ut reprehenderit, eaque
+          tempus at aenean nisi, ea, nibh perferendis.
+          </p>
+
+          <pre><code class="js">
+    var a = 1;
+    var b = 2;
+
+    if (a == 1) {
+      console.log(b);
+    }</code></pre>
+
+          <p>Reprehenderit? Ducimus excepteur aperiam, dis occaecati? Sociosqu
+          metus dis, metus! Cursus excepteur, omnis risus. Mollis fermentum,
+          fermentum labore ullam leo voluptas eleifend doloribus molestias accumsan
+          diamlorem aenean, euismod etiam mauris, ducimus ipsa in inventore,
+          ultricies nihil deserunt minus fugiat! Quae quisque, nemo harum ridiculus
+          sapien in ut reprehenderit, eaque tempus at aenean nisi, ea, nibh
+          perferendis. </p>
+
+        </section>
+
+        <footer class="article__footer">
+        </footer>
+
+      </article>
+
+
+      <article class="article article--excerpt">
+
+        <header class="article__header">
+          <h2>Article title goes here</h2>
+
+          <section class="article__meta">
+            <time datetime="2015-05-16 19:00">November 12, 2015</time>
+            <ul class="article__tags">
+              <li><span class="article__tag">tag 1</span></li>
+              <li><span class="article__tag">another</span></li>
+              <li><span class="article__tag">a longer tag</span></li>
+              <li><span class="article__tag">www</span></li>
+              <li><span class="article__tag">javascript</span></li>
+            </ul>
+          </section>
+        </header>
+
+        <section class="article__body">
+          <p>Faucibus a maiores laborum cillum! Duis debitis vivamus aut proident
+          accusantium nemo, dolorem suscipit! Reiciendis? Commodi! Volutpat
+          possimus, tristique auctor tenetur venenatis ligula laoreet, tempora,
+          nobis massa! Totam vel dolorem minus phasellus perspiciatis! Minim aliqua
+          volutpat consequat possimus fugit accusantium, mauris repellat!
+          Accusantium quo parturient? Molestiae voluptatum ducimus, optio commodo
+          litora dis! Etiam tincidunt! In eiusmod! Litora parturient neque ullamco?
+          Inceptos proident, minima, magnis tortor qui congue donec aliquet wisi
+          rhoncus repellendus provident minima reprehenderit dicta, officia ab per
+          tortor.
+          </p>
+
+          <p>Elit facilis aut augue potenti excepturi ex vehicula imperdiet harum
+          sunt expedita at voluptates massa, odit eget exercitationem adipisicing
+          mus laboris, eros aliquam sagittis! Reprehenderit? Ducimus excepteur
+          aperiam, dis occaecati? Sociosqu metus dis, metus! Cursus excepteur,
+          omnis risus. Mollis fermentum, fermentum labore ullam leo voluptas
+          eleifend doloribus molestias accumsan diamlorem aenean, euismod etiam
+          mauris, ducimus ipsa in inventore, ultricies nihil deserunt minus fugiat!
+          Quae quisque, nemo harum ridiculus sapien in ut reprehenderit, eaque
+          tempus at aenean nisi, ea, nibh perferendis.
+          </p>
+
+          <pre><code class="js">
+    var a = 1;
+    var b = 2;
+
+    if (a == 1) {
+      console.log(b);
+    }</code></pre>
+
+          <p>Reprehenderit? Ducimus excepteur aperiam, dis occaecati? Sociosqu
+          metus dis, metus! Cursus excepteur, omnis risus. Mollis fermentum,
+          fermentum labore ullam leo voluptas eleifend doloribus molestias accumsan
+          diamlorem aenean, euismod etiam mauris, ducimus ipsa in inventore,
+          ultricies nihil deserunt minus fugiat! Quae quisque, nemo harum ridiculus
+          sapien in ut reprehenderit, eaque tempus at aenean nisi, ea, nibh
+          perferendis. </p>
+
+        </section>
+
+        <footer class="article__footer">
+        </footer>
+
+      </article>
+
+
     </main>
 
     <nav class="globalDiv globalNav">
       <ol class="globalNav__postlist">
         <li>
-          <article class="articletile">
+          <article class="articleTile">
+            <h2><a href="#">Another post title</a></h2>
+            <time itemprop="datePublished">Jan 10, 2015</time>
+          </article>
+        </li>
+
+        <li>
+          <article class="articleTile">
+            <h2><a href="#">This is a post title for a post in the nav postlist</a></h2>
+            <time itemprop="datePublished">July 1, 2014</time>
+          </article>
+        </li>
+
+        <li>
+          <article class="articleTile">
             <h2><a href="#">Another post title</a></h2>
             <time itemprop="datePublished">Jan 10, 2015</time>
           </article>
