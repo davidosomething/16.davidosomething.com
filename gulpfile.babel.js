27bd442d90764dff6285e1cc3b504f4e9158766a
David O'Trakoun <dotrakoun@elitedaily.com>
2016-01-12 19:37:45 -0500

    ignore clean-import-paths, on travis master, output eslint for deploy

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -146,11 +146,19 @@ gulp.task('clean', () => {
 
 gulp.task('lint:css', () => {
 
+  const SASSLINT_OPTIONS = {};
+  if (process.env.CI && process.env.TRAVIS_BRANCH === 'master') {
+    SASSLINT_OPTIONS.options = {
+      'formatter':    'html',
+      'output-file':  `${dirs.dist}/docs/sasslint/index.html`,
+    };
+  }
+
   return gulp.src([
     `${dirs.css.source}/**/*.scss`,
     `!${dirs.css.source}/vendor/**/*.scss`,
   ])
-    .pipe(sassLint())
+    .pipe(sassLint(SASSLINT_OPTIONS))
     .pipe(sassLint.format())
     .pipe(sassLint.failOnError());
 
