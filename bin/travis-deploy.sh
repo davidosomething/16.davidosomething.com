327c7d08168bc7e9f2816993b0f1641c2c8b8c30
David O'Trakoun <dotrakoun@elitedaily.com>
2016-01-14 16:13:27 -0500

    use npm run for all ci tasks

--- bin/travis-deploy.sh
+++ bin/travis-deploy.sh
@@ -5,17 +5,25 @@
 set -ev
 
 _deploy() {
-  echo "$GITHUB_PAGES_COMMIT_MESSAGE"
+  echo "--------------------------------------------------------------------------------"
+  echo "Deploying $GITHUB_PAGES_COMMIT_MESSAGE"
+  echo "--------------------------------------------------------------------------------"
+  echo
 
-  npm run deploy-surge &
+  npm run --silent deploy-surge &
 
   openssl aes-256-cbc -K "$encrypted_157bbe5ff2f8_key" -iv "$encrypted_157bbe5ff2f8_iv" -in lib/id_rsa.enc -out ~/.ssh/id_rsa -d
   chmod u=rw,og= ~/.ssh/id_rsa
   echo "Host github.com"              >> ~/.ssh/config
   echo "  IdentityFile ~/.ssh/id_rsa" >> ~/.ssh/config
-  npm run deploy-github
+  npm run --silent deploy-github
 
   wait
+
+  echo
+  echo "--------------------------------------------------------------------------------"
+  echo "Deployed $GITHUB_PAGES_COMMIT_MESSAGE"
+  echo "--------------------------------------------------------------------------------"
 }
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
