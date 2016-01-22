aa3c84ab7902823a305b8b2ec4529db6a5bbc089
David O'Trakoun <me@davidosomething.com>
2016-01-21 23:18:53 -0500

    track click events on some links

---
+++ assets/js/analytics.js
@@ -0,0 +1,59 @@
+/**
+ * Analytics tracking
+ *
+ * @module analytics
+ */
+
+/**
+ * @namespace
+ */
+var analytics = {};
+
+/**
+ * event
+ *
+ * @param {String} description
+ * @param {Object} data
+ * @return {this}
+ */
+analytics.event = (description, data) => {
+  if (!window.ga) {
+    return false;
+  }
+
+  switch (description) {
+
+    case 'Clicked bio profile link':
+      // data = e.target.text
+      window.ga('send', {
+        hitType:        'event',
+        eventCategory:  '.bio__links',
+        eventAction:    description,
+        eventLabel:     `${data}`,
+      });
+      break;
+
+    case 'Clicked footer link':
+      // data = e.target.text
+      window.ga('send', {
+        hitType:        'event',
+        eventCategory:  '.globalFooter',
+        eventAction:    description,
+        eventLabel:     `${data}`,
+      });
+      break;
+
+    case 'Clicked share button':
+      // data = IntentProvider.intentName
+      window.ga('send', {
+        hitType:        'event',
+        eventCategory:  '.widget--sharePost',
+        eventAction:    description,
+        eventLabel:     `${data}`,
+      });
+      break;
+
+  }
+};
+
+export default analytics;
